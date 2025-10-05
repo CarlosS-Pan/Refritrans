6215878fa93500f8ec8dea8c1894a0ad9a8863b8
Carlos Sanabria <carlossanabria636@gmail.com>
2025-10-05 17:18:08 -0400

    Complete hero layout

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -19,10 +19,13 @@ const Navbar = () => {
                 
             </ul>
         </nav>
-        <div className='flex hero' style={{backgroundImage: `url(${HeroSupra})`}}>
+        <div className='flex hero' style={{backgroundImage: `linear-gradient(to top, rgba(0,0,0,1), rgba(0, 0, 0, 0)) , url(${HeroSupra})`}}>
             <div className="flex hero-text">
-                <h2>Expertos en equipos para Refrigeración de Transporte</h2>
+                <h1>Expertos en equipos para Refrigeración de Transporte</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, sint velit? Quas obcaecati maiores voluptate voluptas natus ea quibusdam adipisci tempore. Sed, quos dolor recusandae deleniti dicta eaque quia necessitatibus!</p>
+                <button className='flex link-contact'>
+                    ☺︎ Contáctanos
+                </button>
             </div>
         </div>
         <div>ola amigos de youtube</div>
