e3b0dfe9cedb01eb9a6e6b436fb7be3ab62c0b2c
Carlos Sanabria <carlossanabria636@gmail.com>
2025-11-10 00:48:33 -0500

    Layout grid in Yoe

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,3 +3,6 @@ export { default as HeroSupra } from "./images/Supra_S6-application.png";
 export { default as AboutSupra } from "./images/carrier-supra-right-edit.png";
 export { default as Snowflake } from "./icons/Snowflake.svg";
 export { default as Ice } from "./images/Ice.png";
+export { default as grid1 } from "./images/grid1.png";
+export { default as grid2 } from "./images/grid2.webp";
+export { default as grid4 } from "./images/grid4.jpg";
--- src/components/yoe/Yoe.jsx
+++ src/components/yoe/Yoe.jsx
@@ -1,14 +1,24 @@
 import './yoe.css'
-import { Ice } from "../../assets/index";
+import { Ice, grid1, grid2, grid4 } from "../../assets/index";
 
 function Yoe() {
   return (
     <div className='flex yoe'>
-      <img src={Ice} alt="" />
+      <div className="ice">
+        <img src={Ice} alt="" />
+      </div>
       <div className="pic-grid">
-        <div className="1">1</div>
-        <div className="2">2</div>
-        <div className="3">3</div>
+
+        <div className="bg item-1" style={{backgroundImage: `url(${grid1})`}}>
+        </div>
+        <div className="bg item-2" style={{backgroundImage: `url(${grid4})`}}>
+        </div>
+        <div className="bg item-3" style={{backgroundImage: `linear-gradient(to top, rgba(0,0,0,1), rgba(0, 0, 0, 0))`}}> 10 years
+        </div>
+        <div className="bg item-4" style={{backgroundImage: `url(${grid2})`}}>
+        </div>
+
+
 
       </div>
     </div>
