8e7c282f41ff87120bafe04b26f5125007557f5f
Carlos Sanabria <carlossanabria636@gmail.com>
2025-12-01 02:55:48 -0500

    Style: apply majority of design for service hero section

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,10 +1,11 @@
 export { default as LogoComplete } from "./icons/RefritransLogoCompleteFinal.svg";
-export { default as HeroSupra } from "./images/Supra_S6-application.png";
-export { default as AboutSupra } from "./images/carrier-supra-right-edit.png";
 export { default as Snowflake } from "./icons/Snowflake.svg";
 export { default as Ice } from "./icons/Ice.svg";
+export { default as bgsnowflakes } from "./icons/bg-snowflakes.png";
+
+export { default as HeroSupra } from "./images/Supra_S6-application.png";
+export { default as AboutSupra } from "./images/carrier-supra-right-edit.png";
 export { default as grid1 } from "./images/grid1.png";
 export { default as grid2 } from "./images/grid2.webp";
 export { default as grid4 } from "./images/grid4.jpg";
-export { default as bgsnowflakes } from "./icons/bg-snowflakes.png";
 export { default as servicesPic } from "./images/services-pic.jpg";
--- src/components/ServicesComponents/services-hero/ServicesHero.jsx
+++ src/components/ServicesComponents/services-hero/ServicesHero.jsx
@@ -1,24 +1,48 @@
 import "./serviceshero.css"
+import {servicesPic, Snowflake} from "../../../assets"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faSnowflake } from "@fortawesome/free-regular-svg-icons"
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons"
 
 function ServicesHero() {
   return (
     <div className="flex ser-hero">
         <div className="flex serhero-container">
+            <img src={Snowflake} alt=""/>
             <div className="flex serhero-info-container">
               <div className="serhero-title">
-                title
+                <h1>
+                  Cuidado Integral para tus Sistemas de Refrigeracion en Ruta
+                </h1>
               </div>
               <div className="serhero-info">
-                info
+                <p>
+                  Lorem ipsum dolor sit amet 
+                  consectetur adipisicing elit. 
+                  Molestias non fugit autem deserunt 
+                  voluptate similique laudantium, nesciunt 
+                  odit accusantium eos porro ipsa quo
+                  temporibus vel ex, qui quia, officiis aut?
+                </p>
               </div>
-              <div className="sehero-btn">
-                btn
+              <div className="flex serhero-btn">
+                Contactanos
+                <div className="flex">
+                  <FontAwesomeIcon icon={faArrowRight} size="sm" rotateBy className="btn-rotation" style={{color:"white"}}/>
+                </div>
               </div>
             </div>
-            <div className="serhero-img-container">
-              der
-              <div className="sertag">
-                tagasdasdasdas
+            <div className="serhero-img-container" style={{
+              backgroundImage:`url(${servicesPic})`}}>
+              <div className="flex sertag">
+                <div className="flex serinfo-container">
+                  <p>
+                  Servicios
+                  </p>
+                  <div className="flex">
+                    <FontAwesomeIcon icon={faSnowflake}/>
+                  </div>
+                </div>
               </div>
             </div>
         </div>
