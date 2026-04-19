ba5d07137869447f6ff8e45ad66a6c7685a6f440
Carlos Sanabria <carlossanabria636@gmail.com>
2026-04-19 02:25:29 -0400

    style: fix overflows in homepage

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,9 +2,15 @@ import './navbar.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { LogoComplete } from '../../assets';
 import { faCircleArrowRight } from '@fortawesome/free-solid-svg-icons/faCircleArrowRight';
+import { faBars } from '@fortawesome/free-solid-svg-icons/faBars';
 import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
+
+    // const handleclick =() => {
+         
+    // }
+    
     return (
         <>
         <nav className="flex navbar">
@@ -28,7 +34,37 @@ const Navbar = () => {
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to="/Contacto" className="navbar-btn">
+                    <NavLink to="/Contacto" className="flex navbar-btn">
+                        Contacto
+                        <FontAwesomeIcon icon={faCircleArrowRight} size="lg" rotateBy className='btn-rotation contact-icon-navbar'/>
+                    </NavLink>
+                </li>
+            </ul>
+            <div className="flex burger">
+                <FontAwesomeIcon icon={faBars} size="lg" />
+
+            </div>
+            
+        </nav>
+        <nav className='flex sidebar'>
+            <ul className='flex'>
+                <li className="flex">
+                    <NavLink to="/">
+                     Inicio
+                    </NavLink> 
+                    </li>
+                <li className="flex">
+                    <NavLink to="/Productos">
+                        Productos
+                    </NavLink> 
+                </li>
+                <li className="flex">
+                    <NavLink to="Servicios">
+                        Servicios
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/Contacto" className="flex sidebar-btn">
                         Contacto
                         <FontAwesomeIcon icon={faCircleArrowRight} size="lg" rotateBy className='btn-rotation contact-icon-navbar'/>
                     </NavLink>
